86cbb36b511db07348145e6380ecbdccec3d3b2a
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2021-04-08 12:49:27 +0300

    Create argument checker

---
+++ LibraryApp/Utilities/ArgumentChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LibraryApp.Utilities
+{
+    public class ArgumentChecker : IArgumentChecker
+    {
+        public bool CheckIfAllArgsExists(IDictionary<string, string> passedArgs, string[] requiredArgs)
+        {
+            IList<bool> results = new List<bool>();
+
+            for (int i = 0; i < requiredArgs.Length; i++)
+            {
+                bool requiredArgExists = passedArgs.ContainsKey(requiredArgs[i]) &&
+                    !string.IsNullOrWhiteSpace(passedArgs[requiredArgs[i]]);
+
+                if (requiredArgExists)
+                    results.Add(requiredArgExists);
+            }
+
+            return results.Count == requiredArgs.Length;
+        }
+    }
+}
---
+++ LibraryApp/Utilities/IArgumentChecker.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace LibraryApp.Utilities
+{
+    public interface IArgumentChecker
+    {
+        bool CheckIfAllArgsExists(IDictionary<string, string> passedArgs, string[] requiredArgs);
+    }
+}
